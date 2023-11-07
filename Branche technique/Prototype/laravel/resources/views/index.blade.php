8746c68e8c24c0f70ff236e55740b80a5b2cba03
amine.lamchatab <alamchatab@gmail.com>
2023-11-07 16:59:42 +0100

    configure adminLTE

---
+++ Branche technique/Prototype/laravel/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section("content")
+
+<h1>THI IS INDEX</h1>
+
+@endsection
---
+++ Branche technique/Prototype/laravel/resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    {{-- head --}}
+    @include("layouts.head")
+    {{-- end head --}}
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        {{-- nav --}}
+        @include("layouts.nav")
+        {{-- end nav --}}
+
+
+        {{-- aside --}}
+        @include("layouts.aside")
+        {{-- end aside --}}
+
+
+        {{-- content --}}
+        @yield("content")
+        {{--  end content --}}
+
+
+        {{-- footer --}}
+        @include("layouts.footer")
+        {{-- end footer --}}
+    </div>
+
+    {{-- scripts --}}
+    @include("layouts.script")
+    {{-- end scripts --}}
+</body>
+</html>
+
+
+
+
