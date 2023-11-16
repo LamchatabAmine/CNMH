a748d9f89a1cece66858501aa6242ef8da7111b3
amine.lamchatab <alamchatab@gmail.com>
2023-11-16 09:21:55 +0100

    change the lab-basic and standard

--- Branche technique/Prototype/laravel/resources/views/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer mt-5">
-    <strong>{{__('words.copyright_text')}} &copy; 2023-2024 <a href="#">{{__('words.logo_text')}}</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>{{__('words.Version_text')}}</b> 1.1.0
-    </div>
-</footer>
-
-
