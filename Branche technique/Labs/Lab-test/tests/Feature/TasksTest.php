41eef5aaafdbd0738f661daaec07f24fbaf77238
amine.lamchatab <alamchatab@gmail.com>
2023-12-01 09:51:50 +0100

    complete the test for the methods task

--- Branche technique/Labs/Lab-test/tests/Feature/TasksTest.php
+++ Branche technique/Labs/Lab-test/tests/Feature/TasksTest.php
@@ -13,24 +13,24 @@ class TasksTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        // $this->withoutExceptionHandling();
-        $response = $this->get('/tache/1');
-        // dd($response->status());
-        $response->assertStatus((200));
-        // $response->assertMethodNotAllowed();
-        // $response->assertSee('Aucun tache trouvé.');
-    }
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     // $this->withoutExceptionHandling();
+    //     $response = $this->get('/tache/1');
+    //     // dd($response->status());
+    //     $response->assertStatus((200));
+    //     // $response->assertMethodNotAllowed();
+    //     // $response->assertSee('Aucun tache trouvé.');
+    // }
 
-    public function test_create_task_returns_a_successful_response(): void
-    {
-        // $this->withoutExceptionHandling();
-        $response = $this->get('/tache/1/create');
-        // dd($response->status());
-        $response->assertStatus((200));
-        // $response->assertSee('Aucun tache trouvé.');
-    }
+    // public function test_create_task_returns_a_successful_response(): void
+    // {
+    //     // $this->withoutExceptionHandling();
+    //     $response = $this->get('/tache/1/create');
+    //     // dd($response->status());
+    //     $response->assertStatus((200));
+    //     // $response->assertSee('Aucun tache trouvé.');
+    // }
 
     // public function test_index_task(): void
     // {
